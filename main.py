571fc4780574e5ee79b47469869329525e10b720
pbrazeale <pbrazeale@gmail.com>
2025-05-15 09:55:27 -0500

    0.7.5

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-from graphics import Window, Point, Line
+from graphics import Window
 from cell import Cell
 
 def main ():
     win = Window(800, 600)
 
-
     c = Cell(win)
     c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
+    c.draw_walls(50, 50, 100, 100)
 
     c = Cell(win)
     c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    c.draw_walls(125, 125, 200, 200)
 
     c = Cell(win)
     c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
+    c.draw_walls(225, 225, 250, 250)
 
     c = Cell(win)
     c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
-
+    c.draw_walls(300, 300, 500, 500)
 
     win.wait_for_close()
 
