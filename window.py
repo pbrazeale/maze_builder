846b0d3dd4d5cc01451c7decc7e5b2534aaa00d7
pbrazeale <pbrazeale@gmail.com>
2025-05-15 08:30:33 -0500

    0.3.4

--- window.py
+++ window.py
@@ -6,4 +6,13 @@ class Window():
         self.__root.title("Maze Builder")
         self.__canvas = Canvas(self.__root, width=width, height=height)
         self.__canvas.pack()
+        self.window_on = False
+
+        def redraw(self):
+            self.__root.update_idletasks()
+            self.__root.update()
         
+        def wait_for_close(self):
+            self.window_on = True
+            while self.window_on:
+                
