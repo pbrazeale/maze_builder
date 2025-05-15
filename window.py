8efe922979e56e386cf01adf6f77c19f3d12800c
pbrazeale <pbrazeale@gmail.com>
2025-05-15 08:25:03 -0500

    0.3.2

--- window.py
+++ window.py
@@ -4,4 +4,5 @@ class Window():
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Builder")
+        self.__canvas = Canvas(self.__root, width=width, height=height)
         
