b6df71da2099de719392172e4ffbd05be5f0f9b0
pbrazeale <pbrazeale@gmail.com>
2025-05-15 10:57:50 -0500

    0.9.2

--- cell.py
+++ cell.py
@@ -29,3 +29,19 @@ class Cell():
         if self.has_bottom_wall:
             bottom_wall = Line(Point(x2, y2), Point(x1, y2))
             self._win.draw_line(bottom_wall)
+
+    def draw_move(self, to_cell, undo=False):
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        start_x = int((self._x1 + self._x2) / 2)
+        start_y = int((self._y1 + self._y2) / 2)
+
+        end_x = int((to_cell._x1 + to_cell._x2) / 2)
+        end_y = int((to_cell._y1 + to_cell._y2) / 2)
+
+        connect = Line(Point(start_x, start_y), Point(end_x, end_y))
+        self._win.draw_line(connect, fill_color)
+
+
