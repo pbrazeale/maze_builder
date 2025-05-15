013041fdf48d9632f8563eee1af5ee92c86831fa
pbrazeale <pbrazeale@gmail.com>
2025-05-15 09:53:39 -0500

    0.7.4

--- cell.py
+++ cell.py
@@ -18,22 +18,14 @@ class Cell():
         self._x2 = x2
         self._y2 = y2
         if self.has_left_wall:
-            p1 = 
-            p2 = 
             left_wall = Line(Point(x1, y2), Point(x1, y1))
             self._win.draw_line(left_wall)
         if self.has_top_wall:
-            p1 = Point(x1, y1)
-            p2 = Point(x2, y1)
-            top_wall = Line(p1, p2)
+            top_wall = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(top_wall)
         if self.has_right_wall:
-            p1 = Point(x2, y1)
-            p2 = Point(x2, y2)
-            right_wall = Line(p1, p2)
+            right_wall = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(right_wall)
         if self.has_bottom_wall:
-            p1 = Point(x2, y2)
-            p2 = Point(x1, y2)
-            bottom_wall = Line(p1, p2)
+            bottom_wall = Line(Point(x2, y2), Point(x1, y2))
             self._win.draw_line(bottom_wall)
