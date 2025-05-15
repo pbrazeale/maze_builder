545c3a51c623b2b7ecb2f6127445045cc4b9b6f4
pbrazeale <pbrazeale@gmail.com>
2025-05-15 08:26:30 -0500

    0.3.3

--- window.py
+++ window.py
@@ -5,4 +5,5 @@ class Window():
         self.__root = Tk()
         self.__root.title("Maze Builder")
         self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas.pack()
         
